4795d8f009f3a3a219e0b0c72f8595e10adbe0e9
Keshri Sumit <sumit.keshri@kombee.com>
2025-12-11 09:53:04 +0530

    feat: Introduce base Blade layouts and partials, and add HTTP security headers middleware.

--- app/Http/Middleware/HttpResponseHeaders.php
+++ app/Http/Middleware/HttpResponseHeaders.php
@@ -31,12 +31,13 @@ class HttpResponseHeaders
 
             // Set Content-Security-Policy without nonce and with replace=true
             $csp = "default-src 'self'; " .
-                "script-src 'self' 'unsafe-eval' 'unsafe-inline'; " .
-                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
-                "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
+                "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://code.jquery.com https://www.google.com https://www.gstatic.com; " .
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net; " .
+                "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net; " .
                 "img-src 'self' data:; " .
-                "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; " .
+                "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://fonts.bunny.net data:; " .
                 "connect-src 'self' https://ka-f.fontawesome.com; " .
+                "frame-src 'self' https://www.google.com https://www.gstatic.com; " .
                 "base-uri 'self'; " .
                 "frame-ancestors 'self'; " .
                 "form-action 'self';";
